59472c7f7b8b691918f69de6a4bfc109a4f53dbf
Aleksandr <abirukov2014@yandex.ru>
2022-05-31 14:31:15 +0500

    'delete,add,return'

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,27 +1,53 @@
 import './Main.css'
 import {store} from "../../store/store";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {fetchTasks} from "../../asyncActions/tasks";
 
 export function Main(){
     const task:any = useSelector(state => state)
-    console.log(task.tasks)
+    const dispatch:any = useDispatch()
+    console.log(task)
+    function deleteTask(t:any){
+        dispatch({type: 'DEL_TASK', payload: t.id})
+    }
+    function addTask(t:any){
+
+        const taskNew:any = {
+            userId: 10,
+            id: Math.random(),
+            title: t,
+            completed: false
+        }
+        dispatch({type: 'ADD_TASK', payload:taskNew })
+    }
+
+    function returnTask(){
+        console.log(task)
+        dispatch({type: 'RETURN_TASK', payload: task.returnTask})
+    }
+
     return (
 
         <div className={'main'}>
             <div className={"main__container"}>
-                <div className={"main__button-container"}>
+                <div style={{display:'flex'}} className={"main__button-container"}>
+                    <button onClick={()=> addTask(prompt())} style={{fontSize:"2rem"}}>Добавить задачу!</button>
+                    <button onClick={()=> returnTask()} style={{fontSize:"2rem"}} >Очистить</button>
                 </div>
 
                 <div className={'main__cards'}>
-                    {
+                    { task.tasks.length > 0 ?
                         task.tasks.map((item:any)=>
                             <div className={'main__card'}>
                                 <h3 className={'main__card-name'}>
                                     {item.title}
                                 </h3>
-
+                                <button onClick={()=> deleteTask(item)} style={{fontSize:"3rem"}}>+</button>
                             </div>
-                        )
+                        ) : <div>
+                            Задач нет
+                        </div>
                     }
 
                 </div>
--- src/reducer/reducer.ts
+++ src/reducer/reducer.ts
@@ -1,17 +1,20 @@
 
-
 const defaultState = {
-    tasks: []
+    tasks: [],
+    returnTask: []
 }
 export function reducer(state:any = defaultState, action:any) {
     switch(action.type) {
         case 'GET_TASKS':{
-            return { ...state, tasks: [...state.tasks,...action.payload] };
+            return { ...state, tasks: [...state.tasks,...action.payload], returnTask: [...state.returnTask,...action.payload]};
         }
-
-        case "DEL_CASH":
-            return { ...state, cash: state.cash - action.payload };
-
+        case "ADD_TASK":
+            console.log(action.payload)
+            return { ...state.tasks, tasks: [{...action.payload}, ...state.tasks]  };
+        case "DEL_TASK":
+            return { ...state, tasks: state.tasks.filter((i:any) => i.id !== action.payload) };
+        case "RETURN_TASK":
+            return {...state, tasks: state.tasks = defaultState.tasks}
         default:
             return state;
     }
